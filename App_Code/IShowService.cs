566e0935efd32cbc7c3134dc2a60978942c6309d
Michael <truevc73@gmail.com>
2016-06-11 02:28:46 -0700

    Add files via upload

---
+++ App_Code/IShowService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IShowService" in both code and config file together.
+[ServiceContract]
+public interface IShowService
+{
+    [OperationContract]
+    int VenueLogin(string password, string username);
+    [OperationContract]
+    int VenueRegistration(VenueLite v);
+    [OperationContract]
+    int ArtistAdd(ArtistLite a);
+    [OperationContract]
+    void ShowAdd(ShowLite sl);
+    [OperationContract]
+    void ShowDetailAdd(ShowDetailLite sl);
+
+}
+[DataContract]
+public class ShowLite
+{
+    [DataMember]
+    public string Title { get; set; }
+    [DataMember]
+    public DateTime Date { get; set; }
+    [DataMember]
+    public string Artist { get; set; }
+    [DataMember]
+    public TimeSpan StartTime { get; set; }
+    [DataMember]
+    public string TicketInfo { get; set; }
+
+}
+
+[DataContract]
+public class ShowDetailLite
+{
+    [DataMember]
+    public string Title { get; set; }
+    [DataMember]
+    public string Additional { get; set; }
+    [DataMember]
+    public TimeSpan StartTime { get; set; }
+
+}
+
+[DataContract]
+public class ArtistLite
+{
+    [DataMember]
+    public string artistName { set; get; }
+    [DataMember]
+    public string artistEmail { set; get; }
+    [DataMember]
+    public string artistWeb { set; get; }
+    [DataMember]
+    public DateTime artistDate { set; get; }
+}
+
+[DataContract]
+public class VenueLite
+{
+    [DataMember]
+    public string VenueName { set; get; }
+    [DataMember]
+    public string venueAddress { set; get; }
+    [DataMember]
+    public string venueCity { set; get; }
+    [DataMember]
+    public string venueState { set; get; }
+    [DataMember]
+    public string venueZipCode { set; get; }
+    [DataMember]
+    public string venuePhone { set; get; }
+    [DataMember]
+    public string venueEmail { set; get; }
+    [DataMember]
+    public string venueWebPage { set; get; }
+    [DataMember]
+    public int venueAgeRestriction { set; get; }
+    [DataMember]
+    public string venueLoginUserName { set; get; }
+    [DataMember]
+    public string venuLoginPasswordPlain { set; get; }
+}
---
+++ App_Code/ShowService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ShowService" in code, svc and config file together.
+public class ShowService : IShowService
+{
+    ShowTrackerEntities db = new ShowTrackerEntities();
+    public int ArtistAdd(ArtistLite a)
+    {
+        int result = 1;
+        Artist art = new Artist();
+        art.ArtistName = a.artistName;
+        art.ArtistEmail = a.artistEmail;
+        art.ArtistWebPage = a.artistWeb;
+        art.ArtistDateEntered = a.artistDate;
+        try
+        {
+            db.Artists.Add(art);
+            db.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            result = 0;
+            throw ex;
+        }
+        return result;
+
+    }
+
+    public void ShowAdd(ShowLite sl)
+    {
+        Show s = new Show();
+        s.ShowName = sl.Title;
+        s.ShowDate = sl.Date;
+        s.ShowDateEntered = DateTime.Now;
+        s.ShowTime = sl.StartTime;
+        s.ShowTicketInfo = sl.TicketInfo;
+        db.Shows.Add(s);
+        db.SaveChanges();
+    }
+
+    public void ShowDetailAdd(ShowDetailLite sl)
+    {
+        var skey = from s in db.Shows
+                   where s.ShowName.Equals(sl.Title)
+                   select new { s.ShowKey };
+        int key = 0;
+        foreach (var k in skey)
+        {
+            key = (int)k.ShowKey;
+        }
+        ShowDetail sh = new ShowDetail();
+        sh.ShowDetailArtistStartTime = sl.StartTime;
+        sh.ShowDetailAdditional = sl.Additional;
+        sh.ShowKey = key;
+        db.ShowDetails.Add(sh);
+        db.SaveChanges();
+    }
+
+    public int VenueLogin(string password, string username)
+    {
+        int result = db.usp_venueLogin(username, password);
+        if (result != -1)
+        {
+            var key = from k in db.Venues
+                      where k.VenueName.Equals(username)
+                      select new { k.VenueKey };
+
+            foreach (var k in key)
+            {
+                result = (int)k.VenueKey;
+            }
+        }
+        return result;
+    }
+
+    public int VenueRegistration(VenueLite v)
+    {
+        int result = db.usp_RegisterVenue(
+            v.VenueName, v.venueAddress, v.venueCity,
+             v.venueState, v.venueZipCode, v.venuePhone,
+              v.venueEmail, v.venueWebPage, v.venueAgeRestriction,
+               v.venueLoginUserName, v.venuLoginPasswordPlain);
+        return result;
+    }
+}
